cf17b2b122c9a0c0977cf090217b100622d83e02
Patrick <trickrenzgarcia@gmail.com>
2024-09-03 17:12:57 +0800

    posts

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,7 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Badge } from '@/components/ui/badge'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { fakePosts } from '@/utils/fakePost'
 import { fetchPosts } from '@/utils/fetchPosts'
 import { createFileRoute, useLoaderData } from '@tanstack/react-router'
 
@@ -16,16 +19,31 @@ function Home() {
   console.log(data)
 
   return (
-    <main className='w-full h-[calc(100vh-63px)]'>
+    <main className='w-full'>
       <div className='flex mx-auto max-w-screen-lg'>
-        <Card className='w-full rounded-none'>
-          <CardHeader>
-            <CardTitle>asdasd</CardTitle>
-          </CardHeader>
-          <CardContent>
-            
-          </CardContent>
-        </Card>
+        <ScrollArea className='w-full h-full'>
+          <div className='flex mx-auto flex-col gap-2'>
+            {fakePosts.map((post) => (
+              <Card key={post.id} className='w-full rounded-none'>
+                <CardHeader className='px-4'>
+                  <CardTitle className='text-xl'>{post.title}</CardTitle>
+                  <CardDescription>
+                    <Badge className='rounded-full bg-green-400 hover:bg-green-500'>{post.staff}</Badge> {
+                    post.postedAt.toLocaleDateString('en-US', { dateStyle: 'full' }) +
+                    ' ' + post.postedAt.toLocaleTimeString('en-US', { timeStyle: 'short' })
+                    }
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className='px-0'>
+                  <div className='px-2 lg:px-4'>
+                    {post.body}
+                  </div>
+                  <img className='border' src={post.imageUrl} alt='tite' />
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </ScrollArea>
       </div>
     </main>
   )
