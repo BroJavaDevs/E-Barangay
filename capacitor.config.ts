2cfe205cce07720a9bcd0868833d7c73b952b0c5
Patrick <trickrenzgarcia@gmail.com>
2024-09-05 02:20:14 +0800

    ebarangay

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,10 +2,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'E-Barangay App',
+  appName: 'Brgy82 Announcements',
   webDir: 'build',
   server: {
-    url: 'https://barangay82.brojava.com',
+    url: 'https://barangay82.brojava.com/',
     cleartext: false
   }
 };
